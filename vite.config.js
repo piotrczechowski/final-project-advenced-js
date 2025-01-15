6a9a922536de376082377dfb8dedb5096e3814b2
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2025-01-15 23:14:22 +0100

    add vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,36 @@
 import { defineConfig } from 'vite';
+import {glob} from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  base: 'final-project-advenced-js', 
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    base: '/final-project-advenced-js', 
+    headers: [
+      {
+        "key": "Cross-Origin-Embedder-Policy",
+        "value": "unsafe-none"
+      }
+    ],
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
 });
