b35a2254f412de14c016c4ab0c1030bd0e5cbdb1
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2025-01-17 23:00:33 +0100

    feat: update vite configuration and add git workflow

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,28 @@
-import { defineConfig } from 'vite';
-import handlebars from 'vite-plugin-handlebars';
-import { globSync } from 'glob'; // Correct named import
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
+import { globSync } from "glob"; // Correct named import
 
 // Use globSync to find all HTML files
-const htmlFiles = globSync('./src/*.html');
+const htmlFiles = globSync("./src/*.html");
 
 const input = htmlFiles.reduce((entries, file) => {
-  const name = file.replace('./src/', '').replace('.html', '');
+  const name = file.replace("./src/", "").replace(".html", "");
   entries[name] = file;
   return entries;
 }, {});
 
 export default defineConfig({
-  root: 'src',
+  root: "src",
   build: {
     rollupOptions: {
       input, // Use the generated input object
     },
-    outDir: '../dist', // Ensure output is placed outside 'src'
+    outDir: "../dist", // Ensure output is placed outside 'src'
   },
   plugins: [
     handlebars({
-      partialDirectory: './src/partials', // Path to partials
+      partialDirectory: "./src/partials", // Path to partials
     }),
   ],
+  base: "/final-project-advenced-js/",
 });
