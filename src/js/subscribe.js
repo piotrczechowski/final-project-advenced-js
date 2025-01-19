6f89e2304abb785cb442a386019a0f180f83d20d
piotrczechowski <58622514+piotrczechowski@users.noreply.github.com>
2025-01-19 13:30:34 +0100

    fixing messages from submit form

--- src/js/subscribe.js
+++ src/js/subscribe.js
@@ -1,60 +1,34 @@
-import axios from 'axios';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import { Notifications } from './notifications.js';
+import fetchSportEnergy from './aplication/apiEnergy';
+import { loader } from './utils/loadEmail';
+import { message } from './utils/massage';
 
-const subscriptionForm = document.querySelector('#subscribeForm');
-const emailField = subscriptionForm.querySelector('input[name="email"]');
-const submitButton = document.querySelector('#subscribeBtn');
+const form = document.querySelector('#subscribe-form');
+const input = form.querySelector('#email');
 
-emailField.addEventListener('input', validateEmailField);
-subscriptionForm.addEventListener('submit', handleSubscription);
+form.addEventListener('submit', handlerSendEmail);
 
-function validateEmail(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
-
-function validateEmailField() {
-  const email = emailField.value;
-  const isEmailValid = validateEmail(email);
-
-  emailField.style.borderColor = isEmailValid ? 'green' : 'red';
-}
-
-async function handleSubscription(event) {
+async function handlerSendEmail(event) {
   event.preventDefault();
-  const email = emailField.value;
 
-  if (!validateEmail(email)) {
-    console.error('Invalid email format');
-    Notifications.error('Invalid email address');
+  const { value } = input;
+  const PATTERN = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  PATTERN.test(value);
+
+  if (!PATTERN.test(value)) {
+    message.info("Please provide a valid email address");
+    input.value = '';
     return;
   }
 
-  const formData = new FormData(subscriptionForm);
-  submitButton.disabled = true;
-
-  try {
-    const response = await axios.post(
-      'https://your-energy.b.goit.study/api/subscription',
-      formData,
-      {
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-
-    if (response.status >= 200 && response.status < 300) {
-      subscriptionForm.reset();
-      emailField.style.borderColor = '';
-      Notifications.success(response.data.message);
-    } else {
-      console.error(`Server responded with status: ${response.status}`);
-      Notifications.error(`Server Error: ${response.status} ${response.statusText}`);
-    }
+    try {
+      loader.open()
+    const result = await fetchSportEnergy.addSubscription({ email: value });
+        loader.close()
+        message.success(result.message);
   } catch (error) {
-    console.error('Request failed:', error);
-    Notifications.error(`Error: ${error.message}`);
+    console.log(error);
   } finally {
-    submitButton.disabled = false;
+    input.value = '';
   }
+
 }
