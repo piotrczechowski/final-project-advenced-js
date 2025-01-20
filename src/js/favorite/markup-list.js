7f224da4865c59da666cfd75624f934615408082
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2025-01-20 16:23:33 +0100

    feat: correct favorites list and modal

--- src/js/favorite/markup-list.js
+++ src/js/favorite/markup-list.js
@@ -1,61 +1,60 @@
-import sprite from '../../images/svg/sprite.svg';
-import {cutString} from './slice-string';
+import sprite from "../../images/svg/sprite.svg";
+import { cutString } from "./slice-string";
 
 export function markupList(arr) {
-  const list = document.querySelector('.filter-list-js');
-  list.classList.remove('hidden-form');
+  const list = document.querySelector(".filter-list-js");
+  list.classList.remove("hidden-form");
 
-  if(arr.length > 6){
-    list.classList.add('scroll');
+  if (arr.length > 6) {
+    list.classList.add("scroll");
   } else {
-    list.classList.remove('scroll');
-
+    list.classList.remove("scroll");
   }
 
   const markup = arr
     .map(
       ({ _id, target, name, bodyPart, burnedCalories, time }) => `
-  <li class="favourites_list_item" id=${_id}>
-    <div class="favourites_list_item_up">
-      <div class="favourites_list_item_up_left">
-        <span class="favourites_btn_workout">${cutString(target, 6)}</span>
-        <button id="btnTrash" class="favourites_btn_trash_icon favourites_btn_trash" data-id=${_id}>
-          <svg class="favourites_btn_trash_icon" width="16" height="16" aria-label="trash" data-id=${_id}>
-            <use class="favourites_btn_trash_icon" href="${sprite}#icon-trash" data-id=${_id}></use>
+  <li class="fav-list-item" id=${_id}>
+    <div class="fav-list-item-up">
+      <div class="fav-list-item-up-left">
+        <span class="fav-btn-workout">${cutString(target, 6)}</span>
+        <button id="btnTrash" class="fav-btn-trash-icon fav-btn-trash" data-id=${_id}>
+          <svg class="fav-btn-trash-icon" width="16" height="16" aria-label="trash" data-id=${_id}>
+            <use class="fav-btn-trash-icon" href="${sprite}#icon-trash" data-id=${_id}></use>
           </svg>
         </button>
       </div>
-      <div class="favourites_list_item_up_right">
-        <button class="favourites_btn_start" data-id=${_id}>
-          <p class="favourites_btn_start_text" data-id=${_id}>start</p>
-          <svg class="favourites_btn_start_icon" width="16" height="16" aria-label="arrow" data-id=${_id}>
+      <div class="fav-list-item-up-right">
+        <button class="fav-btn-start" data-id=${_id}>
+          <p class="fav-btn-start-text" data-id=${_id}>start</p>
+          <svg class="fav-btn-start-icon" width="16" height="16" aria-label="arrow" data-id=${_id}>
             <use href="${sprite}#icon-arrow" data-id=${_id}></use>
           </svg>
         </button>
       </div>
     </div>
-    <div class="favourites_list_item_middle">
-      <div class="favourites_list_item_middle_icon">
-        <svg class="favourites_list_item_middle_icon_svg">
+    <div class="fav-list-item-middle">
+      <div class="fav-list-item-middle-icon">
+        <svg class="fav-list-item-middle-icon-svg">
           <use href="${sprite}#icon-run-man"></use>
         </svg>
       </div>
-      <h3 class="favourites_list_item_middle_title">${cutString(name, 16)}</h3>
+      <h3 class="fav-list-item-middle-title">${cutString(name, 16)}</h3>
     </div>
-    <div class="favourites_list_item_bottom">
-      <ul class="favourites_list_item_bottom_list">
-        <li class="favourites_list_item_bottom_list_item">
-          <p class="favourites_list_item_bottom_list_item_text">
+    <div class="fav-list-item-bottom">
+      <ul class="fav-list-item-bottom-list">
+        <li class="fav-list-item-bottom-list-item">
+          <p class="fav-list-item-bottom-list-item-text">
             Burned calories: <span>${burnedCalories}/${time}</span>
           </p>
         </li>
-        <li class="favourites_list_item_bottom_list_item">
-          <p class="favourites_list_item_bottom_list_item_text">
+        <li class="fav-list-item-bottom-list-item">
+          <p class="fav-list-item-bottom-list-item-text">
             Body part: <span>${cutString(bodyPart, 5)}</span>
           </p>
         </li>
-        <li class="favourites_list_item_bottom_list_item">
-          <p class="favourites_list_item_bottom_list_item_text">Target: <span>${cutString(
+        <li class="fav-list-item-bottom-list-item">
+          <p class="fav-list-item-bottom-list-item-text">Target: <span>${cutString(
             target,
             7
           )}</span></p>
@@ -64,7 +63,7 @@ export function markupList(arr) {
     </div>
   </li>`
     )
-    .join('');
+    .join("");
 
-    list.innerHTML = markup;
+  list.innerHTML = markup;
 }
