77dc838a8f6fb4965b5f5afe9cafa98343bca249
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2025-01-20 17:59:57 +0100

    fix: correct favorites card and favicon

--- src/js/modal-window/generation-to-modal.js
+++ src/js/modal-window/generation-to-modal.js
@@ -1,40 +1,40 @@
-import sprite from '../../images/svg/sprite.svg';
-import { capitalizeFirstLetter } from '../utils/firstLater';
+import sprite from "../../images/icons.svg";
+import { capitalizeFirstLetter } from "../utils/firstLater";
 
 function getRating(rating) {
-
-  const lastStar = (rating % 1).toFixed(1)*100
-  let result = '<div class="rating-container-not-cursore" data-rating="0">'
-  for (let i = 1; i <= rating+1; i += 1) {
+  const lastStar = (rating % 1).toFixed(1) * 100;
+  let result = '<div class="rating-container-not-cursore" data-rating="0">';
+  for (let i = 1; i <= rating + 1; i += 1) {
     if (i <= rating) {
       result += `<span class="star-js selected" data-value="${i}"><svg class="exercises_btn_start_icon_modal star-js selected">
               <use xlink:href="${sprite}#icon-star"></use>
               />
-            </svg></span>`
+            </svg></span>`;
     } else {
       if (lastStar > 0) {
         result += `<span class="last-star-js" data-value="${i}"><svg xmlns="http://www.w3.org/2000/svg" width="16" viewBox="0 0 20 19" fill="none">
                     <path d="M9.04894 0.927052C9.3483 0.00574112 10.6517 0.00573993 10.9511 0.927051L12.4697 5.60081C12.6035 6.01284 12.9875 6.2918 13.4207 6.2918H18.335C19.3037 6.2918 19.7065 7.53141 18.9228 8.10081L14.947 10.9894C14.5966 11.244 14.4499 11.6954 14.5838 12.1074L16.1024 16.7812C16.4017 17.7025 15.3472 18.4686 14.5635 17.8992L10.5878 15.0106C10.2373 14.756 9.7627 14.756 9.41221 15.0106L5.43648 17.8992C4.65276 18.4686 3.59828 17.7025 3.89763 16.7812L5.41623 12.1074C5.55011 11.6954 5.40345 11.244 5.05296 10.9894L1.07722 8.10081C0.293507 7.53141 0.696283 6.2918 1.66501 6.2918H6.57929C7.01252 6.2918 7.39647 6.01284 7.53035 5.60081L9.04894 0.927052Z" fill="url(#paint0_linear_126_18474)"/>
                     <defs>
                     <linearGradient id="paint0_linear_126_18474" x1="20" y1="10" x2="-1.99998" y2="10.0209" gradientUnits="userSpaceOnUse">
-                    <stop offset="${100-lastStar}%" stop-color="rgba(244, 244, 244, 0.20)" />
+                    <stop offset="${
+                      100 - lastStar
+                    }%" stop-color="rgba(244, 244, 244, 0.20)" />
                     <stop offset="${lastStar}%" stop-color="#EEA10C" />
                     </linearGradient>
                     </defs>
-                  </svg></span>`
+                  </svg></span>`;
       }
     }
   }
-  for (let i = rating+1; i <= 5; i += 1) {
+  for (let i = rating + 1; i <= 5; i += 1) {
     result += `<span class="star-js" data-value="${i}"><svg class="exercises_btn_start_icon_modal star-js">
               <use xlink:href="${sprite}#icon-star"></use>
               />
-            </svg></span>`
+            </svg></span>`;
   }
 
-  return result +'</div>'
+  return result + "</div>";
 }
-
 
 function getButtonFavorite(isFavorite) {
   if (isFavorite) {
@@ -43,32 +43,35 @@ function getButtonFavorite(isFavorite) {
               <svg class="trash-icon-img" width="15" height="15" aria-label="trash-icon">
                   <use href="${sprite}#icon-trash"></use>
               </svg>
-          </button>`
-    } else {
-      return `<button class="add-favorite-js" type="button" style="font-size: 14px;">
+          </button>`;
+  } else {
+    return `<button class="add-favorite-js" type="button" style="font-size: 14px;">
                 <span>Add to favorites</span>
                 <svg class="heart-icon-img" width="20" height="20" aria-label="heart-icon">
                     <use href="${sprite}#icon-heart"></use>
                 </svg>
-              </button>`
-    }
+              </button>`;
+  }
 }
 
-
 export function getExerciseModal(data) {
-    return `<div class="modal-info" data-id="${data._id}">
+  return `<div class="modal-info" data-id="${data._id}">
      <button class="modal-button-close" id="button-close">
       <svg class="close-icon-img" width="20" height="20" aria-label="close-icon">
         <use href="${sprite}#icon-close"></use>
       </svg>
     </button>
     <div class="modal-image-vrapper">
-      <img loading="lazy" class="modal-img" src="${data.gifUrl}" alt="${data.name}" />
+      <img loading="lazy" class="modal-img" src="${data.gifUrl}" alt="${
+    data.name
+  }" />
     </div>
     <div class="modal-content-wrapper">
       <div class="card-wrapper">
         <h3 class="title-card-modal">${capitalizeFirstLetter(data.name)}</h3>
-        <div class="rating-modal-container-wrapper"><p>${data.rating.toFixed(1)}</p>${getRating(data.rating)}</div>
+        <div class="rating-modal-container-wrapper"><p>${data.rating.toFixed(
+          1
+        )}</p>${getRating(data.rating)}</div>
       </div>
       <hr class="modal-decoration-line" />
       <ul class="modal-table">
@@ -102,11 +105,11 @@ export function getExerciseModal(data) {
           ${getButtonFavorite(data.favorite)}
         </div>
         <button class="add-rating" type="button" style="font-size: 14px;">Give a rating</button>
-      </div>`
+      </div>`;
 }
 
 export function getRatingModal(idExercise) {
-    return `<div class="modal-get-raiting" data-id="${idExercise}">
+  return `<div class="modal-get-raiting" data-id="${idExercise}">
     <button class="modal-button-close" id="button-close">
       <svg class="close-icon-img" width="20" height="20" aria-label="close-icon">
         <use href="${sprite}#icon-close"></use>
@@ -158,5 +161,5 @@ export function getRatingModal(idExercise) {
         <button class="raiting-form-submit" type="submit">Send</button>
       </form>
     </div>
-  </div>`
+  </div>`;
 }
